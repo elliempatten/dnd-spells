e6c7f7a57bf85f55db4266373b3ff1242b59fd99
Ellie Patten <ellie.patten@hotmail.co.uk>
2020-07-02 11:08:38 +0100

    Add client that connects to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import Spells from './example';
 
-class App extends Component {
-  render() {
-    return (
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
       <div className="App">
-        <h1> u suck World! </h1>
+        <h1> Spells </h1>
+        <Spells />
       </div>
-    );
-  }
-}
+    </ApolloProvider>
+  );
+};
 
 export default hot(module)(App);
