bee222bbdb86785820840d8856d9158be9742e45
Ellie Patten <ellie.patten@hotmail.co.uk>
2020-07-02 12:45:21 +0100

    Allow cors

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,13 @@ const { resolvers } = require('./resolvers');
 const PORT = 4000;
 
 const app = express();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+};
 
 const server = new ApolloServer({ typeDefs, resolvers, playground: true });
-server.applyMiddleware({ app });
+server.applyMiddleware({ app, cors: corsOptions });
 
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
